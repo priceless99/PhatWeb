79288fad71e36ac929367412b75ba79d3b6b3a5f
TamarGee <44872655+priceless99@users.noreply.github.com>
2019-02-27 18:04:30 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('myImg');
+var img1 = document.getElementById('myImg1');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = "Converted.jpg" ;
+  captionText.innerHTML = this.alt;
+}
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "Converted(1).jpg" ;
   captionText.innerHTML = this.alt;
 }
 
@@ -18,3 +24,31 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
+
+
+$(document).ready(function(){
+  // Add scrollspy to <body>
+  $('body').scrollspy({target: ".bar", offset: 50});   
+
+  // Add smooth scrolling on all links inside the navbar
+  $("#bar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }  // End if
+  });
+});
