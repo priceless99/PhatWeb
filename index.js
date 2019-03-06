370d3b9ff74ffd9873e0eff1ebd2bfd74dd1ca7e
TamarGee <44872655+priceless99@users.noreply.github.com>
2019-03-06 12:35:59 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -52,3 +52,11 @@ $(document).ready(function(){
     }  // End if
   });
 });
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 50) {
+  $('.navbar').css('background','black');
+  } else {
+  $('.navbar').css('background','transparent');
+  }
+  });
